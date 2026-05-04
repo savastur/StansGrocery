cbf6af4c057fd649a3a2df3ca761a47d08a4f30b
Sturm <savagesturm@gmail.com>
2026-05-03 21:45:00 -0600

    Add project files.

---
+++ StansGrocery/Form1.cs
@@ -0,0 +1,14 @@
+//Sturm Savage
+//RCET2265
+//Spring Semester 2026
+//https://github.com/savastur/CarRental.git
+namespace StansGrocery
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
